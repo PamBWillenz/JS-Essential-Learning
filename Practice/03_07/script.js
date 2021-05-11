39a6c401653048faa58efdd8c4ee93e5b86d5b2f
Pam Willenz <pam@positek.net>
2021-05-11 11:06:35 -0400

    make a bike object, show properties in browser

--- Practice/03_07/script.js
+++ Practice/03_07/script.js
@@ -7,3 +7,32 @@
  * - Find an object that has another object inside of it to create a nested object.
  * - Test your objects in the browser console by accessing the entire object and its specific properties.
  */
+/**
+ * Create a Bike object.
+ */
+
+const bike = {
+  name: "My Roubaix",
+  size: 49,
+  color: "red",
+  component: "Di2",
+  tireSize: {
+    psi: 110,
+    diameter: 720,
+  },
+  electronic: true,
+  gravelAble: function (tireStatus) {
+    this.tireBig = tireStatus;
+  },
+  newTireSize: function (tirePsi, tireDiameter) {
+    this.tireSize.psi = tirePsi;
+    this.tireSize.diameter = tireDiameter;
+  },
+};
+
+console.log("The bike object:", bike);
+console.log("the component value:", bike.component);
+console.log("Tire Size Psi:", bike.tireSize.psi);
+
+var cycle = "component";
+console.log("The component value:", bike[cycle]);
