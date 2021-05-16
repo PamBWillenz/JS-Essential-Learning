3ff6c2625972822199e36b891ded4236cfa62ad1
Pam Willenz <pam@positek.net>
2021-05-16 16:06:10 -0400

    array methods

--- 07_03/script.js
+++ 07_03/script.js
@@ -7,6 +7,37 @@ let backpackContents = ["piggy", "headlamp", "pen"];
 
 console.log(backpackContents);
 
+let roomContents = ["phone", "kitties", "coffee", "wahoo trainer", "briefcase"];
+console.log(roomContents);
+
+// move the last item to the first position
+roomContents = roomContents.filter((item) => item !== "briefcase");
+roomContents.unshift("briefcase");
+console.log(roomContents);
+
+// sort alphabetically
+roomContents.sort();
+console.log(roomContents);
+
+// find a specific item
+roomContents.filter(function (item, index) {
+  console.log(item);
+
+  console.log(index);
+});
+
+// remove an item with a specific value from array
+let value = "kitty";
+let roomContentsTwo = [
+  "phone",
+  "kitties",
+  "coffee",
+  "wahoo trainer",
+  "briefcase",
+];
+roomContentsTwo = roomContentsTwo.filter((item) => item !== value);
+console.log(roomContentsTwo);
+
 // backpackContents.forEach(function (item) {
 //   item = `<li>${item}</li>`;
 //   console.log(item);
--- 07_04/script.js
+++ 07_04/script.js
@@ -2,10 +2,36 @@
  * Challenge: Build and modify an array
  * - Build an array with 8 items
  * - Remove the last item
- * - Add the last item as the first item on the array 
+ * - Add the last item as the first item on the array
  * - Sort the items by alphabetical order
  * - Use the find() method to find a specific item in the array
  * - Remove the item you found using the find method from the array.
  */
 
+// move the last item to the first position
+roomContents = roomContents.filter((item) => item !== "briefcase");
+roomContents.unshift("briefcase");
+console.log(roomContents);
 
+// sort alphabetically
+roomContents.sort();
+console.log(roomContents);
+
+// find a specific item
+roomContents.filter(function (item, index) {
+  console.log(item);
+
+  console.log(index);
+});
+
+// remove an item with a specific value from array
+let value = "kitty";
+let roomContentsTwo = [
+  "phone",
+  "kitties",
+  "coffee",
+  "wahoo trainer",
+  "briefcase",
+];
+roomContentsTwo = roomContentsTwo.filter((item) => item !== value);
+console.log(roomContentsTwo);
