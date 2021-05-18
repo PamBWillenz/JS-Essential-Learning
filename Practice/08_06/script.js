e5278cd557e0d224e0b3a679f1a9613f622fa04a
Pam Willenz <pam@positek.net>
2021-05-18 12:25:16 -0400

    practice functions

--- Practice/08_06/script.js
+++ Practice/08_06/script.js
@@ -4,4 +4,28 @@
  * - Create two functions
  * - Main function creates article element with data from object
  * - Helper function creates.
+ *
  */
+
+const bluePack = {
+  name: "Blue Pack",
+  color: "blue",
+  volume: 10,
+  pocketNum: 5,
+};
+
+const changePack = (presentPack) => {
+  const newArticle = document.createElement("article");
+  newArticle.innerHTML = `
+      <h1>${presentPack.name}</h1>
+      <ul>
+        <li>Volume: ${presentPack.volume}</li>
+        <li>Color: ${presentPack.color}</li>
+        <li>Number of pockets: ${presentPack.pocketNum}</li>
+      </ul>
+    `;
+  return newArticle;
+};
+
+const main = document.querySelector("main");
+main.append(changePack(bluePack));
